dcd84f76af6512e2a1b7c413d16c9d2c008b9d5b
mmodanez <mmodanez@ciandt.com>
2022-07-22 14:51:54 -0300

    CadastrarIngrediente & DescadastrarIngrediente test implementations

--- src/main/java/com/example/armazem/Armazem.java
+++ src/main/java/com/example/armazem/Armazem.java
@@ -6,6 +6,33 @@ import java.util.TreeMap;
 
 public class Armazem {
 
-    private TreeMap<Ingrediente, Integer> estoque;
+    private TreeMap<Ingrediente, Integer> estoque = new TreeMap<>();
 
+    public Armazem(TreeMap<Ingrediente, Integer> estoque) {
+        this.estoque = estoque;
+    }
+
+    public TreeMap<Ingrediente, Integer> getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(TreeMap<Ingrediente, Integer> estoque) {
+        this.estoque = estoque;
+    }
+
+    public void cadastrarIngredienteEmEstoque(Ingrediente ingrediente) {
+        if (estoque.containsKey(ingrediente)) {
+            throw new IllegalArgumentException("_Ingrediente já cadastrado_");
+        } else {
+            estoque.put(ingrediente, 0);
+        }
+    }
+
+    public void descadastrarIngredienteEmEstoque(Ingrediente ingrediente) {
+        if (estoque.containsKey(ingrediente)) {
+            estoque.remove(ingrediente);
+        } else {
+            throw new IllegalArgumentException("_Ingrediente não encontrado_");
+        }
+    }
 }
--- src/test/java/com/example/TDD/TddApplicationTests.java
+++ src/test/java/com/example/TDD/TddApplicationTests.java
@@ -1,15 +1,52 @@
 package com.example.TDD;
 
-import com.example.ingredientes.Ingrediente;
+import com.example.armazem.Armazem;
+import com.example.ingredientes.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.TreeMap;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class TddApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    TreeMap<Ingrediente, Integer> novoEstoque = new TreeMap<>();
+    Armazem estoqueArmazem = new Armazem(novoEstoque);
+
+    @Test
+    void contextLoads() {
+
+    }
+
+    @Test
+    void test_cadastrarIngredienteEmEstoque() {
+        Base novaBase = new Base(TipoBase.Sorvete);
+
+        try {
+            estoqueArmazem.cadastrarIngredienteEmEstoque(novaBase);
+            estoqueArmazem.cadastrarIngredienteEmEstoque(novaBase);
+        } catch
+        (IllegalArgumentException e) {
+            assertEquals(1, estoqueArmazem.getEstoque().size());
+            assertEquals("_Ingrediente já cadastrado_", e.getMessage());
+        }
+    }
+
+    @Test
+    void test_descadastrarIngredienteEmEstoque() {
+        Base novaBase = new Base(TipoBase.Sorvete);
+
+        try {
+            estoqueArmazem.cadastrarIngredienteEmEstoque(novaBase);
+            estoqueArmazem.descadastrarIngredienteEmEstoque(novaBase);
+            estoqueArmazem.descadastrarIngredienteEmEstoque(novaBase);
+        } catch (IllegalArgumentException e) {
+            assertEquals(0, estoqueArmazem.getEstoque().size());
+            assertEquals("_Ingrediente não encontrado_", e.getMessage());
+        }
+    }
 
 
 }
