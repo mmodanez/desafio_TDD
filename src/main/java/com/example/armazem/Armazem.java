ff2fa91401b91eb4ec286bddc6922835c0abd32d
mmodanez <mmodanez@ciandt.com>
2022-07-27 21:04:23 -0300

    Refactor Armazem class methods

--- src/main/java/com/example/armazem/Armazem.java
+++ src/main/java/com/example/armazem/Armazem.java
@@ -20,51 +20,53 @@ public class Armazem {
         this.estoque = estoque;
     }
 
+    private Integer getIngrediente(Ingrediente ingrediente) {
+        return estoque.get(ingrediente);
+    }
+
+    private boolean isContainsKey(Ingrediente ingrediente) {
+        return estoque.containsKey(ingrediente);
+    }
+
     public void cadastrarIngredienteEmEstoque(Ingrediente ingrediente) {
-        if (estoque.containsKey(ingrediente)) {
+        if (isContainsKey(ingrediente))
             throw new IllegalArgumentException("_Ingrediente já cadastrado_");
-        } else {
+        else
             estoque.put(ingrediente, 0);
-        }
     }
 
     public void descadastrarIngredienteEmEstoque(Ingrediente ingrediente) {
-        if (estoque.containsKey(ingrediente)) {
+        if (isContainsKey(ingrediente))
             estoque.remove(ingrediente);
-        } else {
+        else
             throw new IllegalArgumentException("_Ingrediente não encontrado_");
-        }
     }
 
     public void adicionarQuantidadeDoIngredienteEmEstoque(Ingrediente ingrediente, Integer quantidade) {
-        if (estoque.containsKey(ingrediente) && quantidade > 0) {
-            int quantidadeNova = estoque.get(ingrediente) + quantidade;
+        if (isContainsKey(ingrediente) && quantidade > 0) {
+            int quantidadeNova = getIngrediente(ingrediente) + quantidade;
             estoque.put(ingrediente, quantidadeNova);
-        } else {
+        } else
             throw new IllegalArgumentException("_Ingrediente não encontrado ou quantidade inválida_");
-        }
     }
 
     public void reduzirQuantidadeDoIngredienteEmEstoque(Ingrediente ingrediente, Integer quantidade) {
-        if (estoque.containsKey(ingrediente)) {
-            if (estoque.get(ingrediente) >= quantidade && quantidade > 0) {
-                int quantidadeNova = estoque.get(ingrediente) - quantidade;
+        if (isContainsKey(ingrediente)) {
+            if (getIngrediente(ingrediente) >= quantidade && quantidade > 0) {
+                int quantidadeNova = getIngrediente(ingrediente) - quantidade;
                 estoque.put(ingrediente, quantidadeNova);
             }
-        } else {
+        } else
             throw new IllegalArgumentException("_Ingrediente não encontrado ou quantidade inválida_");
-        }
     }
 
     public Integer consultarQuantidadeDoIngredienteEmEstoque(Ingrediente ingrediente) {
         int quantidade = 0;
 
-        if (estoque.containsKey(ingrediente)) {
-            quantidade = estoque.get(ingrediente);
-        }
+        if (isContainsKey(ingrediente))
+            quantidade = getIngrediente(ingrediente);
 
         return quantidade;
     }
-
 
 }
